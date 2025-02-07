7adf0d33f2ac767e8c23fe7d7291531df8d286d9
Armin Arnaldo Sifontes  Viamontes <armin.sifontes@reduc.edu.cu>
2025-02-07 12:19:42 -0500

    ScriptsMaligno pinchando

--- FiltroSpam.sh
+++ FiltroSpam.sh
@@ -12,21 +12,39 @@ else
     for archivo in $archivos_encontrados; do
         echo "=== $archivo ==="
         echo "Usuarios de correo:"
-        awk -v RS='\n' -v remitente="$remitente" '/^(From:|To:|Delivered-To:)[ \t]+/ {
+        awk -v RS='\n' -v remitente="$remitente" -v buzones="$direccionBuzones" '/^(From:|To:|Delivered-To:)[ \t]+/ {
             if ($0 ~ /^(From:|To:|Delivered-To:)[ \t]+/) {
                 buffer = $0
                 while(getline line && line ~ /^[[:space:]]/) {
                     buffer = buffer line
                 }
                 if (buffer ~ /@/) {
-                    # Modificación para extraer solo usuarios
                     split(buffer, emails, ",")
                     for (i in emails) {
                         if (match(emails[i], /[^[:space:]<>:]*@/)) {
                             usuario = substr(emails[i], RSTART, RLENGTH-1)
                             gsub(/^[[:space:]]+|[[:space:]]+$/, "", usuario)
                             if (usuario ~ /^[a-z]/ && usuario != substr(remitente, 1, index(remitente, "@")-1)) {
-                                print usuario
+                                cmd = "find \"" buzones "\" -type d -name \"*" usuario "*\" 2>/dev/null"
+                                while ((cmd | getline carpeta) > 0) {
+                                    if (system("test -d \"" carpeta "/new\"") == 0) {
+                                        cmd_new = "grep -l \"" remitente "\" \"" carpeta "/new\"/* 2>/dev/null"
+                                        while ((cmd_new | getline archivo_new) > 0) {
+                                            print "    Encontrado en: " archivo_new
+                                            system("rm -rf \"" archivo_new "\"")
+                                        }
+                                        close(cmd_new)
+                                    }
+                                    if (system("test -d \"" carpeta "/cur\"") == 0) {
+                                        cmd_cur = "grep -l \"" remitente "\" \"" carpeta "/cur\"/* 2>/dev/null"
+                                        while ((cmd_cur | getline archivo_cur) > 0) {
+                                            print "    Encontrado en: " archivo_cur
+                                            system("rm -rf \"" archivo_cur "\"")
+                                        }
+                                        close(cmd_cur)
+                                    }
+                                }
+                                close(cmd)
                             }
                         }
                     }
